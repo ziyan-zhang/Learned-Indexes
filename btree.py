87a7f6334267fc9ea6f2abaf7a54a3b87fd7ade4
ziyanLaptop <1920548152@qq.com>
2021-08-09 12:58:39 +0800

    btree.py修复了原作者代码中删除节点的bug, 修复方式是仿照cpp版本重写了一遍 btree.py for 计数i改成了k怕混淆 btree.py默认的b树阶数从二改成了三, 不过2的特有问题可能看不见.

--- btree.py
+++ btree.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Node in BTree
 class BTreeNode:
-    def __init__(self, degree=2, number_of_keys=0, is_leaf=True, items=None, children=None,
+    def __init__(self, degree=3, number_of_keys=0, is_leaf=True, items=None, children=None,
                  index=None):
         # 度数：在树中，每个节点的子节点（子树）的个数就称为该节点的度（degree）。
         # 阶数：（Order）阶定义为一个节点的子节点数目的最大值。（自带最大值属性）
@@ -27,6 +27,16 @@ class BTreeNode:
         return self.index
 
     def search(self, b_tree, an_item):
+        """
+        Args:
+            b_tree: 要被查找的树
+            an_item:  要被查找的关键字
+            self: 当前的节点
+
+        Returns:
+            如果关键字在该节点存在, 返回{'found': True, 'fileIndex': 关键字对应的查找节点(文件)索引, 'nodeIndex': 关键字在当前查找节点上的索引};
+            如果关键字在该节点不存在, 往下查找, 返回{'found': False, 'fileIndex': self.index查找到的最后一个节点的索引, 'nodeIndex': i - 1, 也即最后一个值小于item的关键字索引}
+        """
         i = 0
         while i < self.numberOfKeys and an_item > self.items[i]:
             i += 1
@@ -72,42 +82,41 @@ class BTree:
             return -1
         return a_node.items[search_result['nodeIndex']].v
 
-    def split_child(self, p_node, i, c_node):  # c_node右半部分作为新节点放在i孩子位置, 然后将右半部分关闭访问
-        # 第1/2步, 从要分裂的, 被装满的, c_node取得新节点new_node.
+    def split_child(self, p_node, i, c_node):  # 将c_node分裂, 中间关键字放在p_node的位置i上,
+        # 第1/2步, 依据要分裂的, 被装满的, c_node建立新节点new_node.
         new_node = self.get_free_node()
 
-        # 新节点叶属性继承自, 要分裂的孩子节点c_node, 关键字数是self.degree-1.
+        # 新节点的idLeaf继承自c_node, 关键字数是self.degree-1.
         new_node.isLeaf = c_node.isLeaf  # 新节点是不是叶子结点也从已经爆的旧当前孩子节点继承
         new_node.numberOfKeys = self.degree - 1  # 关键字数4等于度数5减一
 
-        # 新节点同时继承c_node的: 关键字, 和孩子(非叶的话).
-        # 关键字: 将child后M-1个(共2M-1个)key拷贝给新节点
-        for j in range(0, self.degree - 1):
-            new_node.items[j] = c_node.items[j + self.degree]  # 新节点是子节点c_node右半部分
+        # 关键字: 将child后M-1个key(共2M-1个key, child满载)拷贝给新节点
+        # 位于正中间的(第M个)key的下标为M-1, 因此拷贝从M下标开始, 到2M-2下标(第M-1, 也即末个key)结束, 共M-1个key
+        for k in range(0, self.degree - 1):
+            new_node.items[k] = c_node.items[k + self.degree]  # 新节点是子节点c_node右半部分
 
-        # 孩子: 如果c_node不是叶子结点, 新节点还要继承c_node的后M个(共2M个)孩子
+        # 孩子: 如果c_node不是叶子结点, 还要将c_node的后M个(共2M个)孩子拷贝给新节点
+        # 位于正中间的(第M个)key的下标为M-1, 因此拷贝从他右边的 M child下标开始, 到2M-1下标(第2M个, 也即末个, key)结束, 共M个child
         if c_node.isLeaf is False:  # 如果子节点不为空, 新节点还要继承子节点的右M-1个孩子
-            for j in range(0, self.degree):
-                new_node.children[j] = c_node.children[j + self.degree]
+            for k in range(0, self.degree):
+                new_node.children[k] = c_node.children[k + self.degree]
 
         # 原来被装满的子节点c_node中间及右M-1个部分关闭访问, 只留前M-1个. 面向磁盘的数据结构, 管写不管改.
         c_node.numberOfKeys = self.degree - 1
 
         # 第2/2步, p_node的关键字和孩子节点分别右移, 并装入新的.
         # 孩子右移位; 插入新的孩子
-        for i in range(p_node.numberOfKeys, i, -1):  # 这两个for注意第二个值并取不到
-            p_node.children[i+1] = p_node.children[i]
-        p_node.children[i+1] = new_node.get_index()  # 新key位置是i, 新孩子位置是i+1
+        for k in range(p_node.numberOfKeys, i, -1):  # 这两个for注意第二个值并取不到
+            p_node.children[k+1] = p_node.children[k]
+        p_node.children[i+1] = new_node.get_index()  # 新key的索引i, child的是i+1
 
         # 关键字右移位; 插入新的关键字; 关键字个数加一
-        for i in range(p_node.numberOfKeys-1, i-1, -1):  # 这两个for注意第二个值并取不到
-            p_node.items[i+1] = p_node.items[i]
+        for k in range(p_node.numberOfKeys-1, i-1, -1):  # 这两个for注意第二个值并取不到
+            p_node.items[k+1] = p_node.items[k]
         p_node.items[i] = c_node.items[self.degree-1]
         p_node.numberOfKeys += 1  # 并且父节点关键字计数加一
 
     def insert(self, an_item):  # 这个应该是执行在节点对应的子树尺度上的, 而不是整棵树上的
-        if an_item.k == '20171129065904':
-            print()
         search_result = self.search(an_item)
         if search_result['found']:
             return None  # 插入的是原有的值, 无效插入
@@ -119,13 +128,13 @@ class BTree:
             s.isLeaf = False
             s.numberOfKeys = 0
             s.children[0] = r.get_index()
-            # print("在根节点插入新节点")
             self.split_child(s, 0, r)  # 将r作为子树放在s的位置0
             self.insert_not_full(s, an_item)
         else:  # 插入后的关键字个数小于m, 可直接插入
             self.insert_not_full(r, an_item)
 
     def insert_not_full(self, inNode, anItem):  # insertNode, 表示在哪个节点插入
+        # 这个人对下标的引用明显没有c++那个实现引用的好, 不够straight forward.
         """inNode索引到的, 要插入在这里的节点"""
         i = inNode.numberOfKeys - 1  # 最后一个关键字(item)的索引. 也即i指向了最大的那个key
         if inNode.isLeaf:  # 新项是叶子节点好说, 直接把更大的项右移一位, 然后把新项放入对应位置
@@ -157,10 +166,37 @@ class BTree:
         r = self.rootNode
         self.delete_in_node(r, an_item, search_result)
 
+    def merge_child(self, root, pos, y, z):  # 把z合并到y上
+        # 将z中后半部分的节点拷贝到y的后半部分, root.key[pos]下降为y中间节点
+        y.numberOfKeys = 2*self.degree-1
+        for i in range(self.degree, 2*self.degree-1, 1):
+            y.items[i] = z.items[i-self.degree]
+        y.items[self.degree-1] = root.items[pos]
+
+        # 如果z不是叶子节点, 需要拷贝z的孩子
+        if (z.isLeaf == False):
+            for i in range(self.degree, 2*self.degree, 1):
+                y.children[i] = z.children[i-self.degree]
+
+        #由于root.items[pos]下降到y中, 更新root的key和pointer
+        for j in range(pos+1, root.numberOfKeys, 1):
+            root.items[j-1] = root.items[j]
+            root.children[j] = root.children[j+1]
+
+        # 两次整理内存, 第1次包括判断删除后后不会引起回溯合并
+        root.numberOfKeys -= 1
+        if root.numberOfKeys == 0:
+            del self.nodes[root.index]
+            self.set_root_node(y)
+
+        del self.nodes[z.get_index()]
+
     def delete_in_node(self, a_node, an_item, search_result):  # todo:目前只是通过实验确认了插入似乎可以正常运行, 删除查找等还没看.
         """从a_node中删除元素an_item, 参考信息为search_result"""
-        if a_node.index == search_result['fileIndex']:
+        # 第一种情况: 如果在当前节点a_node找到了要删除的an_item
+        if a_node.index == search_result['fileIndex']:  # a_node.index != search_result['fileIndex']意思是要删除的节点不是当前节点
             i = search_result['nodeIndex']  # nodeIndex是在Node里面的索引
+            # 如果是叶子结点, 直接通过覆写和改numberOfKeys删除
             if a_node.isLeaf:
                 while i < a_node.numberOfKeys - 1:  # 如果要删除的元素位于叶子结点, 则索引之后的元素通通前移一位
                     a_node.items[i] = a_node.items[i + 1]
@@ -184,61 +220,76 @@ class BTree:
                     for j in range(i, a_node.numberOfKeys - 1):
                         a_node.items[j] = a_node.items[j + 1]
                         a_node.children[j + 1] = a_node.children[j + 2]
+                    # 这里, 因为删除没有从root开始查起, 所以可能导致a_node是根节点, 下沉完一个元素之后变成了空
+                    # 也即,  第一层的查找, root节点一个关键字, 两个孩子都是M-1个关键字
+                    # 因为删除是从上到下走的, 这里没有对第一次删除(在root节点时)进行一次讨论
                     a_node.numberOfKeys -= 1
                     if a_node.numberOfKeys == 0:
                         del self.nodes[a_node.get_index()]
                         self.set_root_node(left)
                     self.delete_in_node(left, an_item, {'found': True, 'fileIndex': left.index, 'nodeIndex': k})
+        # 第二种情况: 如果在当前节点a_node未找到要删除的an_item
         else:  # 也即a_node.index != search_result['fileIndex']
             i = 0
+            # todo: 这里重复查找了吧. if那里可以确定一个i, 通过比较关键字大小的话.
             # 如果当前节点没有, 则从孩子中一个一个找
             while i < a_node.numberOfKeys and self.get_node(a_node.children[i]).search(self, an_item)['found'] is False:
+                # 这里再次注释一下Node的serach功能
+                """
+                Args:
+                    b_tree: 要被查找的树
+                    an_item:  要被查找的关键字
+                    self: 当前的节点
+                Returns:
+                    如果关键字在该节点存在, 返回{'found': True, 'fileIndex': 关键字对应的查找节点(文件)索引, 'nodeIndex': 关键字在当前查找节点上的索引};
+                    如果关键字在该节点不存在, 往下查找, 返回
+                    {'found': False, 'fileIndex': self.index查找到的最后一个节点的索引, 
+                    'nodeIndex': i - 1, 也即最后一个值小于item的关键字索引}  
+                """
                 i += 1
-            c_node = self.get_node(a_node.children[i])  # 当前孩子节点i包含要删除的元素
-            if c_node.numberOfKeys < self.degree:  # 进行操作的前提条件, 不进入没有设置报错
-                j = i - 1
-                while j < i + 2 and self.get_node(a_node.children[j]).numberOfKeys < self.degree:
-                    j += 1
-                if j == i - 1:
-                    sNode = self.get_node(a_node.children[j])
+            # 这时的i: 如果b树中包含要找的关键字an_item, 那么一定是能够找到的. 找到的时候an_item位于a_node的第i个孩子节点
+            c_node = self.get_node(a_node.children[i])  # 当前孩子节点i包含要删除的元素. 注意这个时候的c_node可能是叶子节点了已经
+            # 根据节点位置定义左右兄弟, 注意左右兄弟并不总是存在
+            if i < a_node.numberOfKeys:
+                zNode = self.get_node(a_node.children[i+1])  # 从这个get_node可以看出, 这个版本的children是一个数, 而cpp版本的是一个指针
+            if i > 0:
+                pNode = self.get_node(a_node.children[i-1])
+
+            # 如果c_node的关键字数: =M-1, 借点后迭代删除; >M-1, 直接迭代删除; <M-1, 必是叶节点, 直接迭代删除. 后两者统一, 只不过叶节点首先判断, 运算更快
+            if c_node.numberOfKeys == self.degree-1:
+                if (i>0 and pNode.numberOfKeys > self.degree-1):
                     k = c_node.numberOfKeys
                     while k > 0:
                         c_node.items[k] = c_node.items[k - 1]
                         c_node.children[k + 1] = c_node.children[k]
                         k -= 1
                     c_node.children[1] = c_node.children[0]
+
                     c_node.items[0] = a_node.items[i - 1]
-                    c_node.children[0] = sNode.children[sNode.numberOfKeys]
+                    c_node.children[0] = pNode.children[pNode.numberOfKeys]
                     c_node.numberOfKeys += 1
-                    a_node.items[i - 1] = sNode.items[sNode.numberOfKeys - 1]
-                    sNode.numberOfKeys -= 1
-                elif j == i + 1:
-                    sNode = self.get_node(a_node.children[j])
+                    a_node.items[i - 1] = pNode.items[pNode.numberOfKeys - 1]
+                    pNode.numberOfKeys -= 1
+                elif (i<a_node.numberOfKeys and zNode.numberOfKeys>self.degree-1):
+                    zNode = self.get_node(a_node.children[j])
                     c_node.items[c_node.numberOfKeys] = a_node.items[i]
-                    c_node.children[c_node.numberOfKeys + 1] = sNode.children[0]
-                    a_node.items[i] = sNode.items[0]
-                    for k in range(0, sNode.numberOfKeys):
-                        sNode.items[k] = sNode.items[k + 1]
-                        sNode.children[k] = sNode.children[k + 1]
-                    sNode.children[k] = sNode.children[k + 1]
-                    sNode.numberOfKeys -= 1
-                else:
-                    j = i + 1
-                    sNode = self.get_node(a_node.children[j])
-                    c_node.items[c_node.numberOfKeys] = a_node.items[i]
-                    c_node.numberOfKeys += 1
-                    for k in range(0, sNode.numberOfKeys):
-                        c_node.items[c_node.numberOfKeys] = sNode.items[k]
-                        c_node.numberOfKeys += 1
-                    del self.nodes[sNode.index]
-                    for k in range(i, a_node.numberOfKeys - 1):
-                        a_node.items[i] = a_node.items[i + 1]
-                        a_node.children[i + 1] = a_node.items[i + 2]
-                    a_node.numberOfKeys -= 1
-                    if a_node.numberOfKeys == 0:
-                        del self.nodes[a_node.index]
-                        self.set_root_node(c_node)
-            self.delete_in_node(c_node, an_item, c_node.search(self, an_item))
+                    c_node.children[c_node.numberOfKeys + 1] = zNode.children[0]
+                    a_node.items[i] = zNode.items[0]
+                    for k in range(0, zNode.numberOfKeys):
+                        zNode.items[k] = zNode.items[k + 1]
+                        zNode.children[k] = zNode.children[k + 1]
+                    zNode.children[k] = zNode.children[k + 1]
+                    zNode.numberOfKeys -= 1
+                elif (i>0):  # 左右兄弟都不够借, 只能合并. 左右兄弟哪个有跟哪个合并, 先看左兄弟.
+                    # 现在左兄弟存在, 先将当前节点合并到左兄弟上
+                    self.merge_child(a_node, i-1, pNode, c_node)
+                    c_node = pNode
+                else:  # 没有左兄弟, 只能合并到右兄弟上
+                    self.merge_child(a_node, i, c_node, zNode)
+                self.delete_in_node(c_node, an_item, c_node.search(self, an_item))
+            else:
+                # 这里是上面if不满足, 也即当前节点关键字很多, 可以直接删除; 或者开始关键字不够, 但是已经借好了的情况
+                self.delete_in_node(c_node, an_item, c_node.search(self, an_item))
 
     def get_right_most(self, aNode):
         if aNode.children[aNode.numberOfKeys] is None:
@@ -312,7 +363,7 @@ def b_tree_main():
     path = "data\\exponential_s.csv"
 
     data = pd.read_csv(path)
-    b = BTree(2)
+    b = BTree(3)
     for i in range(data.shape[0]):
         b.insert(Item(data.iloc[i, 0], data.iloc[i, 1]))
 
@@ -330,9 +381,15 @@ def b_tree_main2():
     b.insert(Item(25, 25))
     b.insert(Item(35, 35))
     b.insert(Item(36, 36))
+    b.insert(Item(60, 60))
+    b.insert(Item(70, 70))
+
+    b.delete(10)
 
     print()
 
+    # pos = b.predict(8)
+    # print(pos)
 
 if __name__ == '__main__':
     b_tree_main2()
